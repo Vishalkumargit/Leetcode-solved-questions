e0440a7158afc6342411e916574a6b44167550f3
Vishalkumargit <122820779+Vishalkumargit@users.noreply.github.com>
2023-02-12 11:27:44 +0530

    Time: 4 ms (23.95%), Space: 6 MB (62.28%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-
         int count=0;
-
-    for(int i=0; num > 0 ; i++)
-    {
-      if(num%2 == 0)
-      {
-        num = num/2;
-        count++;
-      }
-      else
-      {
-        num=num-1;
-        count++;
-      }
-    }
-    return count;        
+        while(num!=0){
+        if(num%2==0){
+           num=num/2;
+           count++;
+       }
+        if(num%2!=0){
+            num=num-1;
+            count++;
+        }
+        }
+        return count;
     }
 };
