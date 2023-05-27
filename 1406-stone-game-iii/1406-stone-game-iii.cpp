e483b05456659b9a3c24dce1d28ee3ad8aa725e9
Vishalkumargit <122820779+Vishalkumargit@users.noreply.github.com>
2023-05-27 23:51:24 +0530

    Time: 273 ms (52.61%), Space: 138 MB (32.23%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> prefix(n + 1);
+        for(int i = 0; i < n; i++){
+            prefix[i+1] = prefix[i] + stoneValue[i];
+        }
+        vector<int> dp(n + 4);
+        for(int i = n; i > 0; i--){
+            dp[i] = prefix[n] - prefix[i-1] - min(dp[i+1], min(dp[i+2], dp[i+3]));
+        }
+        if(dp[1]*2 > prefix[n]){
+            return "Alice";
+        }
+        if(dp[1]*2 == prefix[n]){
+            return "Tie";
+        }
+        return "Bob";
+    }
+};
