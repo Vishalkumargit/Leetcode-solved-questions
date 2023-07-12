8bde7396cdafdc69cc2bd0c5d13f7ed6dbde475e
Vishalkumargit <122820779+Vishalkumargit@users.noreply.github.com>
2023-07-12 23:17:29 +0530

    Time: 12 ms (70.34%), Space: 18.4 MB (27.50%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int x = 1; // To check final length of the array
-        
-        // Iterate over the loop
-        for(int i = 1 ;i < nums.size(); i++){
-            // If previous number is equal to this number then the present number is repeated!!!
-            if(nums[i] != nums[i-1]){
-                // so we find all the number once (if they are repeating or not)
-                // and store in the same array
-                nums[x] = nums[i];
-                x++;
-            }
+    int removeDuplicates(vector<int>& arr) {
+    int n =arr.size();
+    int i=0;
+	for(int j=1; j<n; j++)
+	{
+		if(arr[i] != arr[j])
+		{
+			i++;
+
+            arr[i] = arr[j];
+
         }
-        return x;
+
     }
+
+    return i+1;
+    }
+    
 };
